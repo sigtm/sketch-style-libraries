05d23ebd8565459bbc3d9472ba0e9d8da10e6812
Sigurd Mannsaker <sigurd@Sigurds-MBP.lynet.no>
2018-02-07 11:06:19 +0100

    Added proper name to manifest and appcast

--- sketch-style-libraries.sketchplugin/Contents/Sketch/sync-styles.js
+++ sketch-style-libraries.sketchplugin/Contents/Sketch/sync-styles.js
@@ -1,1 +1,211 @@
-var that=this;function __skpm_run(e,t){that.context=t;var n=function(e){var t={};function n(r){if(t[r])return t[r].exports;var a=t[r]={i:r,l:!1,exports:{}};return e[r].call(a.exports,a,a.exports,n),a.l=!0,a.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:r})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){Object.defineProperty(t,"__esModule",{value:!0});var n=function(e){var t=function(e,t){for(var n=[],r=e.count(),a=0;a<r;a++)n[a]=e[a][t]();return n}(e,"name"),n=COSAlertWindow.new(),r=function(e,t){t=t||[],x=t[0]||0,y=t[1]||0,w=t[2]||240,h=t[3]||28;var n=NSMakeRect(x,y,w,h),r=NSComboBox.alloc().initWithFrame(n);return r.addItemsWithObjectValues(e),r.selectItemAtIndex(0),r}(t);return n.addAccessoryView(r),n.setMessageText("Choose a library to sync styles from..."),n.addButtonWithTitle("OK"),n.addButtonWithTitle("Cancel"),n.alert().window().setInitialFirstResponder(r),1e3===n.runModal()?e[r.indexOfSelectedItem()]:-1},r=function(e){for(var t={},n=0;n<e.numberOfSharedStyles();n++){var r=e.objects().objectAtIndex(n);t[r.name()]=r}return t};t.default=function(e){var t=AppController.sharedInstance().librariesController().userLibraries();!function(e,t,n){for(var a=e.documentData(),o=t.documentData(),s=0,u=0,i=["layerStyles","layerTextStyles"],c=0;c<i.length;c++){var l=i[c],d=a[l](),f=r(d),y=o[l](),h=r(y);for(var m in f)h[m]?(y.updateValueOfSharedObject_byCopyingInstance(h[m],f[m].style()),y.synchroniseInstancesOfSharedObject_withInstance(h[m],f[m].style()),u++):(y.addSharedStyleWithName_firstInstance(m,f[m].style()),s++)}var p=s+u;if(p){var b="🤘 "+p+" styles synced";s&&(b+=" ("+s+" new)")}else b="No styles found in the selected library";n.document.showMessage(b)}(n(t).document(),e.document,e)}}]);"default"===e&&"function"==typeof n?n(t):n[e](t)}that.onRun=__skpm_run.bind(this,"default");
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// Pass an array and a property key, and get a new array
+// containing just that property for each item
+//
+var getArrayOfValues = function getArrayOfValues(array, key) {
+  var result = [];
+  var length = array.count();
+
+  for (var i = 0; i < length; i++) {
+    result[i] = array[i][key]();
+  }
+
+  return result;
+};
+
+// Create a ComboBox from an array of values
+//
+var newSelect = function newSelect(array, frame) {
+  frame = frame || [];
+  x = frame[0] || 0;
+  y = frame[1] || 0;
+  w = frame[2] || 240;
+  h = frame[3] || 28;
+
+  var rect = NSMakeRect(x, y, w, h);
+  var combo = NSComboBox.alloc().initWithFrame(rect);
+
+  combo.addItemsWithObjectValues(array);
+  combo.selectItemAtIndex(0);
+
+  return combo;
+};
+
+// Launch popup to pick a library from a set of libraries.
+// Returns the chosen library.
+//
+var pickLibrary = function pickLibrary(libs) {
+
+  var libNames = getArrayOfValues(libs, 'name');
+
+  var alert = COSAlertWindow['new']();
+  var select = newSelect(libNames);
+
+  alert.addAccessoryView(select);
+  alert.setMessageText('Choose a library to sync styles from...');
+  alert.addButtonWithTitle("OK");
+  alert.addButtonWithTitle("Cancel");
+
+  alert.alert().window().setInitialFirstResponder(select);
+
+  var response = alert.runModal();
+
+  if (response === 1000) {
+    return libs[select.indexOfSelectedItem()];
+  } else {
+    return -1;
+  }
+};
+
+// From a set of styles, create an object with the style names as keys
+//
+var getStylesByName = function getStylesByName(styles) {
+  var result = {};
+
+  for (var i = 0; i < styles.numberOfSharedStyles(); i++) {
+    var style = styles.objects().objectAtIndex(i);
+    result[style.name()] = style;
+  }
+
+  return result;
+};
+
+// Copy layer and text styles from one document to another,
+// updating any that already exist by the same name
+//
+var copyStylesFromDocument = function copyStylesFromDocument(source, dest, context) {
+
+  var sourceData = source.documentData();
+  var destData = dest.documentData();
+  var newCount = 0;
+  var updateCount = 0;
+
+  var _arr = ['layerStyles', 'layerTextStyles'];
+  for (var _i = 0; _i < _arr.length; _i++) {
+    var type = _arr[_i];
+
+    var sourceStyles = sourceData[type]();
+    var sourceStylesByName = getStylesByName(sourceStyles);
+
+    var destStyles = destData[type]();
+    var destStylesByName = getStylesByName(destStyles);
+
+    for (var name in sourceStylesByName) {
+      if (destStylesByName[name]) {
+        destStyles.updateValueOfSharedObject_byCopyingInstance(destStylesByName[name], sourceStylesByName[name].style());
+        destStyles.synchroniseInstancesOfSharedObject_withInstance(destStylesByName[name], sourceStylesByName[name].style());
+        updateCount++;
+      } else {
+        destStyles.addSharedStyleWithName_firstInstance(name, sourceStylesByName[name].style());
+        newCount++;
+      }
+    }
+  }
+
+  var totalCount = newCount + updateCount;
+
+  if (totalCount) {
+    var message = '🤘 ' + totalCount + ' styles synced';
+    if (newCount) message += ' (' + newCount + ' new)';
+  } else {
+    var message = 'No styles found in the selected library';
+  }
+
+  context.document.showMessage(message);
+};
+
+var syncStyles = function syncStyles(context) {
+
+  var userLibs = AppController.sharedInstance().librariesController().userLibraries();
+  var lib = pickLibrary(userLibs);
+
+  copyStylesFromDocument(lib.document(), context.document, context);
+};
+
+exports['default'] = syncStyles;
+
+/***/ })
+/******/ ]);
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onRun'] = __skpm_run.bind(this, 'default')
